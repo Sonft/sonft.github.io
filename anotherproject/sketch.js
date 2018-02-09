58e1e8ca0588938878e00b2bdc06ec197fb0263a
Sonft <36053930+Sonft@users.noreply.github.com>
2018-02-09 14:45:54 -0600

    anotherproject

---
+++ anotherproject/sketch.js
@@ -0,0 +1,17 @@
+// Jerry's project
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
+
+function draw() {
+  //Hero
+  ellipse(mouseX,mouseY,,[h])
+
+
+
+}
