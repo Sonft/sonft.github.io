16f0cec2095e11f72ab706c55fd990cd9f594980
Sonft <36053930+Sonft@users.noreply.github.com>
2018-02-13 15:12:37 -0600

    changeproject

--- anotherproject/sketch.js
+++ anotherproject/sketch.js
@@ -13,14 +13,15 @@ function draw() {
   fill(255, 204, 0);
   ellipse(mouseX, mouseY, 20);
   // ellipse(mouseX,mouseY,30)
-
+  writeSomething();
+  
 }
 
-// function mouseIsPressed() {
-//   if mouseClicked() {
-//     fill(255, 204, 0);
-//     textFont()
-//     textSize(32)
-//     text("this is sunshine");
-//   }
-// }
+function writeSomething() {
+  textSize(32);
+  text('word', 10, 30);
+  fill(0, 102, 153);
+  text('word', 10, 60);
+  fill(0, 102, 153, 51);
+  text('word', 10, 90);
+}
