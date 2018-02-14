964976a43c4472ca87737df04f642daff63ca550
Sonft <36053930+Sonft@users.noreply.github.com>
2018-02-14 15:17:01 -0600

    works

--- anotherproject/sketch.js
+++ anotherproject/sketch.js
@@ -12,22 +12,23 @@ function draw() {
   background(0);
   fill(255, 204, 0);
   ellipse(mouseX, mouseY, size);
-  writeSomething();
-
+  if (numberOfClicks >= 10) {
+    writeSomething();
+  }
 }
-//
-//
-// function mouseClicked() {
-//   // numberOfClicks += 1
-//   // if (numberOfClicks <= 1) {
-//     writeSomething();
-//   }
+
+
+function mouseClicked() {
+  numberOfClicks += 1;
+  size += 0.5;
+}
 
 function writeSomething() {
   textSize(32);
-  text('word', 10, 30);
+  fill(255);
+  text("word", 10, 30);
   fill(0, 102, 153);
-  text('word', 10, 60);
+  text("word", 10, 60);
   fill(0, 102, 153, 51);
-  text('word', 10, 90);
+  text("word", 10, 90);
 }
